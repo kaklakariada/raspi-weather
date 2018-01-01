087fac1540ae78b1fb6f0a685f95a6503699fda0
kaklakariada <kaklakariada@users.noreply.github.com>
2018-01-01 10:36:10 +0100

    Use hystrix when writing to db

--- src/main/java/weather/upload/InfluxService.java
+++ src/main/java/weather/upload/InfluxService.java
@@ -1,13 +1,18 @@
 package weather.upload;
 
-import java.util.concurrent.TimeUnit;
-
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDB.ConsistencyLevel;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
 
 public class InfluxService {
+	private static final Logger LOG = LoggerFactory.getLogger(InfluxService.class);
+
 	private static final String DEFAULT_RETENTION_POLICY = "autogen";
 	private final InfluxDB influxDB;
 
@@ -19,12 +24,39 @@ public class InfluxService {
 		final InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086")
 				.enableGzip()
 				.setConsistency(ConsistencyLevel.QUORUM)
-				.setDatabase(null)
-				.enableBatch(10, 10, TimeUnit.SECONDS);
+				.setDatabase(null);
 		return new InfluxService(influxDB);
 	}
 
 	public void write(String database, Point point) {
-		influxDB.write(database, DEFAULT_RETENTION_POLICY, point);
+		new WriteDbCommand(influxDB, database, point).queue();
+	}
+
+	private static class WriteDbCommand extends HystrixCommand<Void> {
+
+		private final InfluxDB influxDB;
+		private final String database;
+		private final Point point;
+
+		private WriteDbCommand(InfluxDB influxDB, String database, Point point) {
+			super(HystrixCommandGroupKey.Factory.asKey("influxdb"));
+			this.influxDB = influxDB;
+			this.database = database;
+			this.point = point;
+		}
+
+		@Override
+		protected Void run() throws Exception {
+			LOG.trace("Writing point to db {}...", database);
+			influxDB.write(database, DEFAULT_RETENTION_POLICY, point);
+			LOG.trace("Wrote point to db {} successfully", database);
+			return null;
+		}
+
+		@Override
+		protected Void getFallback() {
+			LOG.warn("Writing to db {} failed for point {}: ignore", database, point);
+			return null;
+		}
 	}
 }
