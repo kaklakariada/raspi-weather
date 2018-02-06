ca4ca4474713318f459af3314f71a063dbc8e600
kaklakariada <kaklakariada@users.noreply.github.com>
2018-02-06 21:14:43 +0100

    Connect again when fritzbox token becomes invalid

--- src/main/java/weather/sensor/fritzdect/FritzDectService.java
+++ src/main/java/weather/sensor/fritzdect/FritzDectService.java
@@ -2,50 +2,60 @@ package weather.sensor.fritzdect;
 
 import java.time.Clock;
 import java.time.Instant;
+import java.util.function.Supplier;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.github.kaklakariada.fritzbox.HomeAutomation;
+import com.github.kaklakariada.fritzbox.http.AccessForbiddenException;
+import com.github.kaklakariada.fritzbox.model.homeautomation.DeviceList;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 
 public class FritzDectService {
 	private static final Logger LOG = LoggerFactory.getLogger(FritzDectService.class);
 	private static final int READ_TIMEOUT_MILLIS = 5 * 1000;
-	private final HomeAutomation homeAutomation;
+	private HomeAutomation homeAutomation;
+	private final Supplier<HomeAutomation> homeAutomationSupplier;
 	private final Clock clock;
 
-	FritzDectService(HomeAutomation homeAutomation, Clock clock) {
-		this.homeAutomation = homeAutomation;
+	FritzDectService(Supplier<HomeAutomation> homeAutomationSupplier, Clock clock) {
+		this.homeAutomationSupplier = homeAutomationSupplier;
+		this.homeAutomation = homeAutomationSupplier.get();
 		this.clock = clock;
 	}
 
 	public static FritzDectService create(String url, String user, String password) {
 		LOG.info("Logging in to '{}' with username '{}'", url, user);
-		final HomeAutomation homeAutomation = HomeAutomation.connect(url, user, password);
-		return new FritzDectService(homeAutomation, Clock.systemUTC());
+		return new FritzDectService(() -> HomeAutomation.connect(url, user, password), Clock.systemUTC());
 	}
 
 	public FritzDectMeasurment getMeasurment() {
-		return new GetFritzDectMeasurmentCommand(homeAutomation, clock).execute();
+		return new GetFritzDectMeasurmentCommand().execute();
 	}
 
-	private static class GetFritzDectMeasurmentCommand extends HystrixCommand<FritzDectMeasurment> {
+	private class GetFritzDectMeasurmentCommand extends HystrixCommand<FritzDectMeasurment> {
 
-		private final HomeAutomation homeAutomation;
-		private final Clock clock;
-
-		private GetFritzDectMeasurmentCommand(HomeAutomation homeAutomation, Clock clock) {
+		private GetFritzDectMeasurmentCommand() {
 			super(HystrixCommandGroupKey.Factory.asKey("fritzbox"), READ_TIMEOUT_MILLIS);
-			this.homeAutomation = homeAutomation;
-			this.clock = clock;
 		}
 
 		@Override
-		protected FritzDectMeasurment run() throws Exception {
+		protected FritzDectMeasurment run() {
+			try {
+				return getMeasurment();
+			} catch (final AccessForbiddenException e) {
+				LOG.warn("Token is invalid, reconnect", e);
+				homeAutomation = homeAutomationSupplier.get();
+				return getMeasurment();
+			}
+		}
+
+		private FritzDectMeasurment getMeasurment() {
 			final Instant timestamp = clock.instant();
-			return new FritzDectMeasurment(timestamp, homeAutomation.getDeviceListInfos());
+			final DeviceList deviceListInfo = homeAutomation.getDeviceListInfos();
+			return new FritzDectMeasurment(timestamp, deviceListInfo);
 		}
 
 		@Override
